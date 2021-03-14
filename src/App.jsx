6feb0a932bca509d85b1682da2a016bada17c009
megu3k <49593780+megu3k@users.noreply.github.com>
2021-03-14 16:21:58 +0900

    add html

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
-import React from "reacr";
+import React from "react";
 import "./styles.css";
 
 export const App = () => {
   return (
     <>
-      <div></div>
-      <div></div>
-      <div></div>
+      <div>
+        <input placeholder="TODO入力" />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のTODO</p>
+        <ul>
+          <div>
+            <li>タスクです</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+          <div>
+            <li>タスクです</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+      <div>
+        <p>完了のTODO</p>
+        <ul>
+          <div>
+            <li>タスクです</li>
+            <button>戻す</button>
+          </div>
+        </ul>
+      </div>
     </>
   );
 };
